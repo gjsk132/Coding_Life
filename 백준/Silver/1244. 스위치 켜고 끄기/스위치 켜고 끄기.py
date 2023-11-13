bef3d2ee3a4f1a4caf6be105495218f196c8992b
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-11-13 22:38:28 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,4 +1,3 @@
-# open(0).readline == sys.stdin.readline
 input = open(0).readline
 
 switch = int(input())
@@ -9,25 +8,23 @@ for i in range(people):
     gender, num = map(int, input().split())
     
     if gender == 1:
-        for j in range(num, switch+1, num):
-            status[j - 1] ^= 1
+        for j in range(num-1, switch, num):
+            status[j] ^= 1
         
     elif gender == 2:
-        for j in range(min(num,switch-num+1)):
-            if status[num+j-1] and status[num-j-1]:
-                status[num+j-1] = 0
-                status[num-j-1] = 0
-            elif not status[num+j-1] and not status[num-j-1]:
-                    status[num+j-1] = 1
-                    status[num-j-1] = 1
-            else:
-                break
+        status[num-1] ^= 1
+        left = num-2;
+        right = num;
+        while  left>-1 and right<switch and status[left] == status[right]:
+            status[left] ^= 1
+            status[right] ^= 1
+            left -= 1;
+            right += 1;
     
 cnt = 0
 for i in status[0:]:
     cnt += 1
-    if cnt != 20:
-        print(i, end=' ')    
-    else:
-        print(i)
+    print(i, end=' ')    
+    if cnt == 20:
+        print()
         cnt = 0
