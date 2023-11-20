b756f62455f0e83493b0eac3e8096daf682d5852
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-11-21 00:04:41 +0900

    [Silver IV] Title: 수 찾기, Time: 188 ms, Memory: 50520 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,10 @@
 input = open(0).readline
 
 n = int(input())
-nlist = list(map(int,input().split()))
-nlist.sort()
+nlist = set(map(int,input().split()))
 
 cnt = int(input())
 targets = list(map(int,input().split()))
 
-def find_num(left, right, target):
-    
-    mid = (left + right)//2
-    
-    if (left > right):
-        print("0")
-        return
-        
-    if target > nlist[mid]:
-        find_num(mid+1, right, target)
-    elif target < nlist[mid]:
-        find_num(left, mid-1, target)
-    else:
-        print("1")
-        return
-
 for target in targets:
-    find_num(0,len(nlist)-1, target)
+    print(1) if target in nlist else print(0)
