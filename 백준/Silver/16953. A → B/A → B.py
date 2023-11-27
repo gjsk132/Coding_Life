47f75664c9918e1eb48a1e6479345b4dea24000f
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-11-27 20:38:11 +0900

    [Silver II] Title: A → B, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -2,14 +2,12 @@ input = open(0).readline
 num, target = map(int,input().split())
 cnt = 0
 while target > num:
-    if (target%2==0):
+    if target%2==0:
         target //= 2
-        cnt+=1
-        continue
-    elif (target%10==1):
+    elif target%10==1:
         target //= 10
-        cnt+=1
     else:
         break
-
+    cnt += 1
+    
 print(cnt+1 if target==num else -1)
