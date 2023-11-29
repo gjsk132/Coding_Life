3b3fd7166798fdb3c84330dd00acfa6067308cf7
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-11-30 03:10:18 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,17 +1,16 @@
-from collections import deque
-
 input = open(0).readline
 people = int(input())
-deq = deque([])
+size = []
 
 for i in range(people):
-    deq.append(list(map(int, input().split())))
+    size.append(list(map(int, input().split())))
 
 for i in range(people):
-    now = deq.popleft()
     rank = 1
-    for j in deq:
-        if now[0] < j[0] and now[1] < j[1]:
+    for j in range(people):
+        if i == j:
+            continue
+        elif size[i][0] < size[j][0] and size[i][1] < size[j][1]:
             rank += 1
-    deq.append(now)
-    print(rank, end=" ")
+            
+    print(rank, end =" ")
