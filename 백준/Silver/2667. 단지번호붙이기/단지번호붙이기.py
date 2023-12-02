147675cce1780adf32470dd8f552bf0738e3e32d
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-12-03 05:13:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 109412 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,10 +1,8 @@
 input = open(0).readline
-
 size = int(input())
 chart = [list(map(int, input().rstrip("\n"))) for i in range(size)]
 
 area = []
-cnt = 0
 
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
@@ -26,9 +24,8 @@ def dfs(x, y):
 for my in range(size):
     for mx in range(size):
         if chart[my][mx] == 1:
-            cnt += 1
             area.append(dfs(mx, my))
 
     
-print(cnt)
+print(len(area))
 print(*sorted(area), sep="\n")
