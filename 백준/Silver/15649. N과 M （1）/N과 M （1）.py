a17bf96d96f3211fa11c01c25b0b83a95e1f73be
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-12-03 06:34:05 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,4 @@
-n, m = map(int, input().split())    
-
-mark = []
-
-def back(cnt):
-    if cnt == 0:
-        print(*mark)
-        return
-    
-    for i in range(1, n+1):
-        if not i in mark:
-            mark.append(i)
-            back(cnt-1)
-            mark.pop()
-            
-back(m)
+from itertools import permutations
+n, m = map(int, input().split())
+for i in map(list, permutations(range(1, n+1), m)):
+    print(*i)
