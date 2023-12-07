9c50398de4f787aaa0a77b40d5c9141282673e79
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-12-07 21:48:04 +0900

    [Bronze II] Title: 분해합, Time: 1188 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,9 +2,8 @@ import sys
 t = int(sys.stdin.readline())
 for i in range(1, t+2):
     if i==t+1:
-        print(0)
         break
     n = i + sum(map(int,str(i)))
     if n == t:
-        print(i)
         break
+print(0 if i==t+1 else i)
