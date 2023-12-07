2ce15d145a4533c40eef2024dec6d2bb4156a833
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-12-07 22:42:38 +0900

    [Silver III] Title: 최애의 팀원, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/29813. 최애의 팀원/최애의 팀원.py
+++ 백준/Silver/29813. 최애의 팀원/최애의 팀원.py
@@ -5,7 +5,9 @@ members = [list(input().split()) for _ in range(member)]
 
 while len(members) != 1:
     now = members.pop(0)
-    index = (int(now[1])-1)%len(members)
+    index = int(now[1])%len(members)-1
+    if index==-1:
+        index += len(members)
     members.pop(index)
     members = members[index:] + members[:index]
     
