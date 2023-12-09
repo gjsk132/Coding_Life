e2ff8445d3924622ef0057d8379d15920240346c
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-12-10 00:38:40 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -2,7 +2,4 @@ while True:
     n = input()
     if n == '0':
         break
-    if n == "".join(reversed(n)):
-        print("yes")
-    else:
-        print("no")
+    print("yes" if n=="".join(reversed(n)) else "no")
