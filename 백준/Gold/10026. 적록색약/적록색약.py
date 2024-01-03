f1c54bafa59ecca7aded158680c145b5087e79d5
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-03 22:39:42 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 32748 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -6,18 +6,15 @@ size = int(input())
 area = [list(input().strip()) for i in range(size)]
 blind = [['G' if j=='R' else j for j in i] for i in area]
 
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
 def bfs(x, y, color):
-    for x_, y_ in zip(dx,dy):
-        nx = x+x_
-        ny = y+y_
+    area[x][y] = 'X'
+    for dx, dy in zip([-1,0,1,0],[0,1,0,-1]):
+        nx = x+dx
+        ny = y+dy
         if nx < 0 or ny < 0 or nx == size or ny == size:
             continue
         if not area[nx][ny] == color:
             continue
-        area[nx][ny] = 'X'
         bfs(nx,ny,color)
         
 def findarea():
