113050a0df32df5b6b44b8a475fd3140308001e1
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-07 18:09:24 +0900

    [Silver III] Title: 두 수의 합, Time: 96 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,24 +1,23 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 input = open(0).readline
 size = int(input())
 num = sorted(list(map(int,input().split())))
-goal = int(input())
+target = int(input())
 
-def find(start,end,target):
-    if start >= end:
-        return 0
+start, end, result = 0, size-1, 0
+
+while start < end:
+     
+    add = num[start] + num[end]  
+    
+    if add > target:
+        end -= 1
         
-    if num[start] + num[end] > target:
-        return find(start, end-1, target)
+    elif add < target:
+        start += 1
+        
+    else:
+        start += 1
+        end -= 1
+        result += 1
     
-    elif num[start] + num[end] < target:
-        return find(start+1, end, target)
-    
-    elif num[start] + num[end] == target:
-        return 1+find(start+1,end-1,target)
-    
-    return
-    
-print(find(0,size-1,goal))
+print(result)
