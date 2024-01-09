55b04c94ed9f734186942f769d52b92030700881
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-09 16:26:02 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,30 +1,24 @@
 input = open(0).readline
+cnt = int(input())
+state = list(map(int,input().split()))
 
-switch = int(input())
-status = list(map(int, input().split()))
-people = int(input())
-
-for i in range(people):
-    gender, num = map(int, input().split())
+for i in range(int(input())):
+    gender, num = map(int,input().split())
     
     if gender == 1:
-        for j in range(num-1, switch, num):
-            status[j] ^= 1
-        
-    elif gender == 2:
-        status[num-1] ^= 1
-        left = num-2;
-        right = num;
-        while  left>-1 and right<switch and status[left] == status[right]:
-            status[left] ^= 1
-            status[right] ^= 1
-            left -= 1;
-            right += 1;
-    
-cnt = 0
-for i in status[0:]:
-    cnt += 1
-    print(i, end=' ')    
-    if cnt == 20:
+        for i in range(num-1,cnt,num):
+            state[i] ^= 1
+    else:
+        left = num-2
+        right = num
+        state[num-1] ^= 1
+        while left > -1 and right < cnt and state[left]==state[right]:
+            state[left] ^= 1
+            state[right] ^= 1
+            left -= 1
+            right += 1
+
+for k, v in enumerate(state):
+    print(v, end=" ")
+    if not (k+1)%20:
         print()
-        cnt = 0
