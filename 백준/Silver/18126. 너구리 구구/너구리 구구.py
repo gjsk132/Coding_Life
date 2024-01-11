1af56f5a442f3e46f2adc3cd7bd982fa06542b95
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-12 06:15:15 +0900

    [Silver II] Title: 너구리 구구, Time: 3168 ms, Memory: 236036 KB -BaekjoonHub

---
+++ 백준/Silver/18126. 너구리 구구/너구리 구구.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+size = int(input())
+
+dist = [[0 for i in range(size+1)] for _ in range(size+1)]
+
+for i in range(size-1):
+    a, b, c = map(int,input().split())
+    dist[a][b] = dist[b][a] = c
+
+dq = deque([(1,0)])
+
+check = [False]*(size+1)
+
+result = 0
+
+while dq:
+    now, cost = dq.popleft()
+    
+    if not check[now]:
+        check[now] = True
+        for k, v in enumerate(dist[now]):
+            if (not v == 0) and (not check[k]):
+                dq.append([k, cost+v])
+                result = max(result, cost+v)
+print(result)
