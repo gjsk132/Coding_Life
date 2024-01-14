4d4bc70c92ee4f740ab1d5557c8f2c25d2b1fe6b
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-15 02:57:34 +0900

    [Gold V] Title: 내려가기, Time: 4736 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,15 @@
+layer = int(input())
+
+low = [[0,0,0], [0,0,0]]
+high = [i[:] for i in low]
+
+for i in range(layer):
+    p = i%2
+    low[p] = list(map(int,input().split()))
+    high[p] = [_ for _ in low[p]]
+    
+    for j in range(3):
+        low[p][j] += min(1e9 if j==0 else low[p-1][j-1], low[p-1][j],1e9 if j==2 else low[p-1][j+1])
+        high[p][j] += max(0 if j==0 else high[p-1][j-1], high[p-1][j],0 if j==2 else high[p-1][j+1])
+
+print(max(high[layer%2-1]), min(low[layer%2-1]))
