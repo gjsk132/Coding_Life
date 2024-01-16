bef95e59dc938a10530b84da44cac6052a2bff5d
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-16 23:35:44 +0900

    [Gold III] Title: 치즈, Time: 176 ms, Memory: 114700 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,61 +1,37 @@
 from collections import deque
-
+input = open(0).readline
 n, m = map(int,input().split())
 
 table = [[i for i in map(int,input().split())] for _ in range(n)]
 
 melt = deque()
-
 empty = deque([(0,0,0)])
 table[0][0] = -1
 
 result = 0
-cnt = 0
 
 while empty or melt:
-    
     if empty:
         px, py, day = empty.popleft()
-        for dx, dy in zip([-1,0,1,0],[0,1,0,-1]):
-            nx = px+dx
-            ny = py+dy
-            
-            if nx < 0 or ny < 0 or nx == n or ny == m or table[nx][ny] == -1:
-                continue
-            
-            if table[nx][ny] == 0:
-                table[nx][ny] = -1
-                empty.append([nx, ny, day])
-            
-            elif table[nx][ny] == 2:
-                table[nx][ny] = -1
-                melt.append([nx, ny, day+1])
-                cnt += 1
-                
-            else:
-                table[nx][ny] += 1
-    
     else:
         px, py, day = melt.popleft()
         result = max(day, result)
         
-        for dx, dy in zip([-1,0,1,0],[0,1,0,-1]):
-            nx = px+dx
-            ny = py+dy
+    for dx, dy in zip([-1,0,1,0],[0,1,0,-1]):
+        nx = px+dx
+        ny = py+dy
+        
+        if nx < 0 or ny < 0 or nx == n or ny == m or table[nx][ny] == -1:
+            continue
+        
+        if table[nx][ny] == 0:
+            table[nx][ny] = -1
+            empty.append([nx, ny, day])
+        
+        elif table[nx][ny] == 2:
+            table[nx][ny] = -1
+            melt.append([nx, ny, day+1])
             
-            if table[nx][ny] == -1:
-                continue
-            
-            if table[nx][ny] == 0:
-                table[nx][ny] = -1
-                empty.append([nx, ny, day])
-                
-            elif table[nx][ny] == 2:
-                table[nx][ny] = -1
-                melt.append([nx, ny, day+1])
-                cnt += 1
-                
-            else:
-                table[nx][ny] += 1
-    
+        else:
+            table[nx][ny] += 1
 print(result)
