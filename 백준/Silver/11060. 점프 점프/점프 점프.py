290cf0309af5102b812fdb3aa4e4ef8672f5c86a
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-29 01:26:40 +0900

    [Silver II] Title: 점프 점프, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -2,7 +2,7 @@ input = open(0).readline
 n = int(input())
 dp = [0]+[1001]*(n-1)
 
-for point, area in zip(range(n), map(int,input().split())):
+for point, area in enumerate(map(int,input().split())):
     for i in range(point+1, point+area+1):
         if i >= n:
             break
