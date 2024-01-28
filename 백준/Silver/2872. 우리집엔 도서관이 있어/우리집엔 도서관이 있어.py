8d9dbc8a1864f40461bdba995044f343e398a235
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-29 02:54:34 +0900

    [Silver II] Title: 우리집엔 도서관이 있어, Time: 188 ms, Memory: 42876 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.py
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.py
@@ -1,15 +1,10 @@
 input = open(0).readline
 
 n = int(input())
-books = []
-
-for i in range(n):
-    books.append(now := int(input()))    
+books = [int(input()) for i in range(n)]
 
 now = books.index(n)
-
 limit = max(books[now+1:] if now!=n-1 else [0])
-
 result = limit
 
 while now > 0 :
