c5ef3364d8ce6b27c9f8dcb3aa0690da36c02ce0
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-02-01 19:25:44 +0900

    [Gold V] Title: A와 B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,10 @@
+target = input()
+words = list(input())
+
+for i in range(len(words)-len(target)):
+    if (n := words.pop()) == 'A':
+        continue
+    else:
+        words.reverse()
+
+print(1 if target == "".join(words) else 0)
