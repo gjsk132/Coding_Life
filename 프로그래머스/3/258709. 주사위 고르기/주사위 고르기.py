21e66d4a0d15f7833705571a1d32d2460e4c47ce
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-02-14 04:25:48 +0900

    [level 3] Title: 주사위 고르기, Time: 2031.44 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,26 +1,14 @@
 from itertools import combinations, product
-from collections import defaultdict, deque
+from collections import defaultdict
 
 
 def get_count_of_each_combo(dices, indices):
     num_combos: Dict[int, int] = defaultdict(int)
-    
     for pips in product(range(6), repeat=len(indices)):
         sum_combo: int = sum(dices[i][j] for i, j in zip(indices, pips))  # i번째 주사위 j번째 눈
         num_combos[sum_combo] += 1
-        
-    return num_combos
+    return sorted(num_combos.items())
 
-def change_sort_list(combo):
-    return sorted(combo.items())
-
-def make_prefix_sum(combo, cnt):
-    num_combos, limit = combo, max(combo.keys())
-    for i in range(2,cnt):
-        num_combos[i] += num_combos[i-1]
-    
-    return num_combos
-    
 
 def solution(dice):
     answer: List[int] = []
@@ -29,16 +17,34 @@ def solution(dice):
     dice_indices: Set[int] = set(range(n))
     max_win_cnt: int = 0
     for A in combinations(range(n), n // 2):
+        
+        if A[0]:
+            break
+        
         B: Set[int] = dice_indices.difference(A)
         
-        A_combos: List[Tuple[int, int]] = change_sort_list(get_count_of_each_combo(dice, A))
-        B_combos: Dict[int, int] = make_prefix_sum(get_count_of_each_combo(dice, B), A_combos[-1][0])
-        win_cnt: int = 0
-        for A_combo_sum, A_combo_cnt in A_combos:
-            win_cnt += A_combo_cnt * B_combos[A_combo_sum-1]
+        A_combos: List[Tuple[int, int]] = get_count_of_each_combo(dice, A)
+        B_combos: List[Tuple[int, int]] = get_count_of_each_combo(dice, B)
         
-        if win_cnt > max_win_cnt:
-            max_win_cnt = win_cnt
+        A_win_cnt: int = 0
+        B_win_cnt: int = 6**n
+        
+        for A_combo_sum, A_combo_cnt in A_combos:
+            for B_combo_sum, B_combo_cnt in B_combos:
+                if B_combo_sum >= A_combo_sum:
+                    B_win_cnt -= (A_combo_cnt * B_combo_cnt) if B_combo_sum == A_combo_sum else 0
+                    break
+                    
+                A_win_cnt += A_combo_cnt * B_combo_cnt
+        
+        B_win_cnt -= A_win_cnt
+        
+        if A_win_cnt > max_win_cnt:
+            max_win_cnt = A_win_cnt
             answer = A
+            
+        if B_win_cnt > max_win_cnt:
+            max_win_cnt = B_win_cnt
+            answer = B
         
     return [i + 1 for i in answer]
