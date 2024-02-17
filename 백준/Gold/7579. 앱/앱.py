fa2c964b9b89345b56b9cb649918cc36951b8414
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-02-18 06:21:58 +0900

    [Gold III] Title: 앱, Time: 396 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -7,7 +7,7 @@ limit = 10**4
 sum_cost = [0 for _ in range(limit+1)]
 
 for use_m, use_c in zip([m for m in map(int,input().split())],[c for c in map(int,input().split())]):
-    for now_c in reversed(range(use_c, limit+1)):
+    for now_c in range(limit, use_c -1, -1):
         sum_cost[now_c] = max(sum_cost[now_c], sum_cost[now_c-use_c]+use_m)
 
 for i in range(limit+1):
