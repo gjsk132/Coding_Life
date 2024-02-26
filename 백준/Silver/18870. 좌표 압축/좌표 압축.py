dbc32e66fb8933b3b18fbbd81192c273d916aa16
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-02-27 06:52:12 +0900

    [Silver II] Title: 좌표 압축, Time: 2044 ms, Memory: 148028 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+cnt = int(input())
+
+origin = list(map(int,input().split()))
+
+num = sorted(set(origin))
+
+num_dict = defaultdict(int)
+
+for k, v in enumerate(num):
+    num_dict[v] = k
+
+for i in origin:
+    print(num_dict[i], end=" ")
