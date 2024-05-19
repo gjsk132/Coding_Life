c1d82bd296cfe6f7d94c7054f318e01c0310bc16
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-05-20 01:58:18 +0900

    [Silver I] Title: 숫자 재배치, Time: 328 ms, Memory: 158988 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
@@ -1,32 +1,21 @@
-from collections import defaultdict
+from itertools import permutations
 
 input = open(0).readline
 
-given_num, limit = map(int,input().split())
-num_cnt = defaultdict(int)
-num_len = 0
+A, B = input().split()
+num_A = list(A)
+perm_A = list(permutations(num_A, len(A)))
 
-for num in str(given_num):
-    num_cnt[num] += 1
-    num_len += 1
+ans = -1
+check = False
+for i in perm_A:
+    i_list = []
+    for j in i:
+        i_list.append(j)
+    if i_list[0] == '0':
+        continue
 
-num_kinds = sorted(num_cnt.keys(), reverse=True)
-num_combs = []
+    if int(''.join(i_list)) < int(B):
+        ans = max(ans, int(''.join(i_list)))
 
-def backtracking():
-    if len(num_combs) == num_len:
-        return int("".join(map(str, num_combs)))
-    
-    for n in num_kinds:
-        if num_cnt[n] == 0:
-            continue
-        num_combs.append(n)
-        num_cnt[n] -= 1
-        result = backtracking()
-        if result and result < limit:
-            return result
-        num_combs.pop()
-        num_cnt[n] += 1
-
-answer = backtracking()
-print(-1 if len(str(answer)) != num_len else answer)
+print(ans)
