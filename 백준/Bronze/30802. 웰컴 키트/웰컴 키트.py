2038b6771e9f798754ac6848ea5f5bb8473a2b37
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-11-13 16:09:50 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -0,0 +1,15 @@
+input = open(0).readline
+
+N = int(input())
+sizes = list(map(int, input().split()))
+T, P = map(int, input().split())
+
+need_t = 0
+
+for s in sizes:
+    need_t += s//T
+    need_t += 1 if s%T else 0
+
+print(need_t)
+
+print(N//P, N%P)
