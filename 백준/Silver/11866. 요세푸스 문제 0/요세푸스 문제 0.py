e517b6be00fc458b653ecae180aec0c9b145a277
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-11-17 01:23:34 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 136 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+input = open(0).readline
+
+N, K = map(int,input().split())
+
+answer = []
+
+nums = deque([i+1 for i in range(N)])
+
+pos = 1
+
+while nums:
+    n = nums.popleft()
+    
+    if not (pos := pos%K) == 0:
+        nums.append(n)
+    else:
+        answer.append(str(n))
+
+    pos += 1
+
+print("<", end="")
+print(", ".join(answer), end=">")
