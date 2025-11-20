a88bf3fb9847f139a0213f00ce93bc1b4ed06e7b
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-11-20 21:04:45 +0900

    [Bronze II] Title: 분산처리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,20 @@
+input = open(0).readline
+
+n = int(input())
+
+last = {0:"10", 1:"1", 2:"2486", 3:"3971", 4:"46", 5:"5", 6:"6", 7:"7931", 8:"8426", 9:"91"}
+
+def answer(a, b):
+
+    a %= 10
+    b -= 1
+
+    if a == 0:
+        print(10)
+    else:
+        print(last[a][b%len(last[a])])
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    
+    answer(a, b)
