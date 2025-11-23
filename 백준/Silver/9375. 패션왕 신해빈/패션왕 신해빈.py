d7b5107f6b3a3694329372dcf9de976a38102bca
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-11-23 18:01:33 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 52 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+input = open(0).readline
+
+def calculate_cases(clothes_cnt):
+    clothes = defaultdict(int)
+
+    for _ in range(clothes_cnt):
+        c_name, c_sort = input().split()
+
+        clothes[c_sort] += 1
+    
+    answer = 1
+    
+    for v in clothes.values():
+        answer *= (v+1)
+
+    return answer-1
+
+cases = int(input())
+
+for _ in range(cases):
+    c_cnt = int(input())
+
+    print(calculate_cases(c_cnt))
