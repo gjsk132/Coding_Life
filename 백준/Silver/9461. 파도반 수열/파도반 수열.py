b3fbd71dad483090099ff9e5d7e737dc34ea17aa
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-11-24 19:16:46 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,15 @@
+input = open(0).readline
+
+cases = int(input())
+
+cases_list = [int(input()) for _ in range(cases)]
+
+limit = max(cases_list)+1
+
+dp = [1 for _ in range(limit)]
+
+for i in range(4, limit):
+    dp[i] = dp[i-3]+dp[i-2]
+
+for c in cases_list:
+    print(dp[c])
