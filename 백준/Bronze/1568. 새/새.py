db6f9784b529ca9c419c5b2bc8bd850462dac523
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-11-28 23:49:29 +0900

    [Bronze II] Title: 새, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1568. 새/새.py
@@ -0,0 +1,18 @@
+input = open(0).readline
+
+sing_num = 1
+
+answer = 0
+
+left = int(input())
+
+while not left == 0:
+    if sing_num > left:
+        sing_num = 1
+        continue
+    
+    left -= sing_num
+    sing_num += 1
+    answer += 1
+    
+print(answer)
