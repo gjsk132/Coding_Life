735c433b1b4c0ed7d57a5272d1d45bc55bca7ee3
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-07 01:38:31 +0900

    [Bronze II] Title: 저항, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,11 @@
+input = open(0).readline
+
+resistance = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+answer = 0
+
+answer += resistance.index(input().strip())*10 + resistance.index(input().strip())
+
+answer = answer * pow(10, resistance.index(input().strip()))
+
+print(answer)
