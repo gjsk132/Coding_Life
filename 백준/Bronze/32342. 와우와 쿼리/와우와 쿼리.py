37917de8751982a97d9a8d6491ed53eb75e093d8
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-08 05:46:14 +0900

    [Bronze III] Title: 와우와 쿼리, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32342. 와우와 쿼리/와우와 쿼리.py
@@ -0,0 +1,15 @@
+input = open(0).readline
+
+cnt = int(input())
+
+target = "WOW"
+
+for _ in range(cnt):
+    string = input().strip()
+    answer = 0
+
+    for i in range(0, len(string)-2):
+        if string[i:i+3] == target:
+            answer += 1
+    
+    print(answer)
