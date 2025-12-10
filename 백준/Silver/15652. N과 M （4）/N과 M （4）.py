1488cf5c50118dad2c0511bc330e49071ec36d9b
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-11 00:18:39 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+input = open(0).readline
+
+N, M = map(int, input().split())
+
+stack = []
+
+def recursion(pre, cnt):
+    if cnt == M:
+        print(" ".join(map(str, stack)))
+        return
+    
+    for i in range(pre, N+1):
+        stack.append(i)
+        recursion(i, cnt+1)
+        stack.pop()
+
+recursion(1, 0)
