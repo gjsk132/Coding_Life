180dd72121ffe73985c552ae7d2740eeabcc6342
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-17 17:23:27 +0900

    [Bronze III] Title: Nadan, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16239. Nadan/Nadan.py
+++ 백준/Bronze/16239. Nadan/Nadan.py
@@ -3,9 +3,8 @@ input = open(0).readline
 total = int(input())
 cnt = int(input())
 
-tmp = [i+1 for i in range(cnt)]
+for i in range(cnt-1):
+    print(i+1)
+    total -= i+1
 
-tmp[-1] += total - sum(tmp)
-
-for t in tmp:
-    print(t)
+print(total)
