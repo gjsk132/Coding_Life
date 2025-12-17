7c7a2e5290d9e6c3c068b50d8e402ce10f0fcadb
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-17 17:21:50 +0900

    [Bronze III] Title: Nadan, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16239. Nadan/Nadan.py
+++ 백준/Bronze/16239. Nadan/Nadan.py
@@ -5,16 +5,7 @@ cnt = int(input())
 
 tmp = [i+1 for i in range(cnt)]
 
-now = sum(tmp)
-
-while now < total:
-    for i in range(cnt):
-        if not i == cnt-1 and tmp[i+1] == tmp[i] + 1:
-            continue
-        else:
-            tmp[i] += 1
-            break
-    now += 1
+tmp[-1] += total - sum(tmp)
 
 for t in tmp:
     print(t)
