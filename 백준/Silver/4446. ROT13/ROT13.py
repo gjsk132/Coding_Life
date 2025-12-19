549bb585c8f202efe061c06d19f019a71f7f1d69
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-20 06:10:45 +0900

    [Silver V] Title: ROT13, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4446. ROT13/ROT13.py
@@ -0,0 +1,30 @@
+input = open(0).readline
+
+before_vowels = "aiyeou"
+before_consonants = "bkxznhdcwgpvjqtsrlmf"
+
+resolve_rules = {}
+
+for before, after in zip(before_vowels, before_vowels[3:]+before_vowels[:3]):
+    resolve_rules[before] = after
+    resolve_rules[before.upper()] = after.upper()
+
+for before, after in zip(before_consonants, before_consonants[10:]+before_consonants[:10]):
+    resolve_rules[before] = after
+    resolve_rules[before.upper()] = after.upper()
+
+while True:
+    try:
+        line = input()
+
+        if not line:
+            break
+
+        for ch in line:
+            if ch in resolve_rules:
+                print(resolve_rules[ch], end="")
+            else:
+                print(ch, end="")
+
+    except:
+        break
