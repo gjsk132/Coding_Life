e5576bf55df4d3521d112406c51d8eaa6efa3bb0
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-20 06:55:11 +0900

    [Silver IV] Title: 블록 놀이, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16951. 블록 놀이/블록 놀이.py
@@ -0,0 +1,21 @@
+input = open(0).readline
+
+cnt, favor_num = map(int, input().split())
+
+towers_height = list(map(int, input().split()))
+
+answer = float('inf')
+
+for ti in range(cnt):
+    minute = 0
+
+    if towers_height[ti]-ti*favor_num < 1:
+        continue
+
+    for i, th in enumerate(towers_height):
+        if not towers_height[ti]+(i-ti)*favor_num == th:
+            minute += 1
+
+    answer = min(answer, minute)
+
+print(answer)
