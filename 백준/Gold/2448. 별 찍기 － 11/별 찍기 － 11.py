f1bb65677034a3b136602d6d5526b9fbe931ef14
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-21 06:14:00 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 116 ms, Memory: 54588 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -0,0 +1,31 @@
+input = open(0).readline
+
+lines = int(input())
+
+shape = ["  *  ", " * * ","*****"]
+
+def level_up(pre):
+
+    now = len(pre)//3 * 2
+    
+    length = now*5+(now-1)
+    side_gap = (length//2 + 1)//2
+
+    next_shape = []
+
+    for s in shape:
+        next_shape.append(" "*side_gap+s+" "*side_gap)
+    
+    for s in shape:
+        next_shape.append(s+" "+s)
+    
+    return next_shape
+
+while not len(shape) == lines:
+    shape = level_up(shape)
+
+for i, s in enumerate(shape):
+    if i == lines-1:
+        print(s)
+    else:
+        print(s, end=" \n")
