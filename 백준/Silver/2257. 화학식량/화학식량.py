360171791e20064a2559aac46184e319be31e5b5
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-21 05:43:13 +0900

    [Silver I] Title: 화학식량, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2257. 화학식량/화학식량.py
@@ -0,0 +1,31 @@
+input = open(0).readline
+
+elements = input().strip()
+
+value = {
+    "H" : 1,
+    "C" : 12,
+    "O" : 16,
+    "(" : 0
+}
+
+stack = []
+
+for i in range(len(elements)):
+    e = elements[i]
+    
+    if e.isdigit():
+        stack.append(stack.pop()*int(e))
+
+    elif not e == ")":
+        stack.append(value[e])
+    else:
+        while stack:
+            if (a:=stack.pop()) == 0:
+                break
+            elif (b:=stack.pop()) == 0:
+                stack.append(a)
+                break
+            else:
+                stack.append(a+b)            
+print(sum(stack))
