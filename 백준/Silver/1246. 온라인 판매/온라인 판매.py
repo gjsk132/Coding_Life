b3130cb231dc78999fd3e7e36819b2adac1ddfcd
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2025-12-30 04:03:57 +0900

    [Silver IV] Title: 온라인 판매, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1246. 온라인 판매/온라인 판매.py
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.py
@@ -1,17 +1,15 @@
 input = open(0).readline
 
 N, M = map(int, input().split())
-
 prices = sorted([int(input()) for _ in range(M)])
 
-answer_price = 0
-answer_total = 0
+p = 0
+t = 0
 
 for i, price in enumerate(prices):
     cnt = min(N, M-i)
+    if price*cnt > t:
+        p = price
+        t = price*cnt
 
-    if price*cnt > answer_total:
-        answer_price = price
-        answer_total = price*cnt
-
-print(answer_price, answer_total)
+print(p, t)
