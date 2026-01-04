ffcac2070227c07b9b34d12cfb0c53f5c03763c8
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-01-05 00:57:19 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,27 @@
+input = open(0).readline
+
+score = {
+    "A+" : 4.5,
+    "A0" : 4.0,
+    "B+" : 3.5,
+    "B0" : 3.0,
+    "C+" : 2.5,
+    "C0" : 2.0,
+    "D+" : 1.5,
+    "D0" : 1.0,
+    "F" : 0.0
+}
+
+answer = 0.0
+cnt = 0
+
+for _ in range(20):
+    name, grade, value = input().split()
+    if value == "P":
+        continue
+    
+    grade = float(grade)
+    answer += score[value]*grade
+    cnt += grade
+
+print(answer/cnt)
