dd0176ff2983b0c849c46c37e3bddbe71abaf1eb
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-01-07 03:50:51 +0900

    [Silver V] Title: 효정과 새 모니터, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/20949. 효정과 새 모니터/효정과 새 모니터.py
@@ -0,0 +1,8 @@
+input = open(0).readline
+
+N = int(input())
+
+monitors = [list(map(int, input().split()))+[i+1] for i in range(N)]
+
+for m in sorted(monitors, key=lambda x: x[0]**2+x[1]**2, reverse=True):
+    print(m[2])
