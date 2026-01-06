6b69b67253d0c125a66afe140286bb689267027b
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-01-07 02:49:24 +0900

    [Bronze III] Title: Arithmetic and Geometric Sums, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9310. Arithmetic and Geometric Sums/Arithmetic and Geometric Sums.py
@@ -0,0 +1,12 @@
+input = open(0).readline
+
+while n:=int(input()):
+    a1, a2, a3 = map(int, input().split())
+
+    if a3+a1 == a2+a2:
+        d = a2-a1
+        print((n)*(2*a1+(n-1)*d)//2)
+        
+    else:
+        r = a2//a1
+        print((a1)*(pow(r, n)-1)//(r-1))
