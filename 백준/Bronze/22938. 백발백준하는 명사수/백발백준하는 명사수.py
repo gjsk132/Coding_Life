0e7b45c2f28c2837c4649f666b08d3133f2c714d
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-01-08 17:47:36 +0900

    [Bronze III] Title: 백발백준하는 명사수, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/22938. 백발백준하는 명사수/백발백준하는 명사수.py
@@ -0,0 +1,10 @@
+x1, y1, r1 = map(int, input().split())
+x2, y2, r2 = map(int, input().split())
+
+a = pow((x2-x1), 2) + pow((y2-y1), 2)
+b= pow((r1+r2), 2) 
+
+if  a < b:
+    print("YES")
+else:
+    print("NO")
