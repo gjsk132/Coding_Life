bdac4e1b494543d1a19be3877a0697d884180300
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-01-25 02:58:50 +0900

    [Silver V] Title: 상품의 주인은?, Time: 1320 ms, Memory: 89888 KB -BaekjoonHub

---
+++ 백준/Silver/24509. 상품의 주인은？/상품의 주인은？.py
@@ -0,0 +1,19 @@
+input = open(0).readline
+
+student_cnt = int(input())
+
+# id, score
+prizes = [[0, 0]]*4
+
+score_table = [list(map(int, input().split())) for _ in range(student_cnt)]
+
+prize_student_id = [0, 0, 0, 0]
+
+for subject_idx in range(4):
+    score_table = sorted(score_table, key=lambda x: (x[subject_idx+1], -x[0]))
+
+    prize_student_info = score_table.pop()
+
+    prize_student_id[subject_idx] = prize_student_info[0]
+
+print(" ".join(map(str, prize_student_id)))
