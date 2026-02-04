b8f3d0e0cf196234cbf10a650906a2380ba064aa
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-02-04 18:11:41 +0900

    [Bronze I] Title: 우유가 넘어지면?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17363. 우유가 넘어지면？/우유가 넘어지면？.py
@@ -0,0 +1,23 @@
+input = open(0).readline
+
+offset = {
+    "." : ".",
+    "O" : "O",
+    "-" : "|",
+    "|" : "-",
+    "/" : "\\",
+    "\\" : "/",
+    "^" : "<",
+    "<" : "v",
+    "v" : ">",
+    ">" : "^"
+}
+
+row, col = map(int, input().split())
+
+data = [input().strip() for _ in range(row)]
+
+for y in range(col):
+    for x in range(row):
+        print(offset[data[x][col-y-1]], end="")
+    print()
