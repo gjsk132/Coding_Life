e1e99f51f69970b09c8a212184708eb704898c3c
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-02-15 23:31:44 +0900

    [Bronze I] Title: 2007년, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -0,0 +1,11 @@
+input = open(0).readline
+
+day_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+date = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+
+m, d = map(int, input().split())
+
+day_cnt = sum(day_in_month[:m-1]) + d-1
+
+print(date[day_cnt%7])
