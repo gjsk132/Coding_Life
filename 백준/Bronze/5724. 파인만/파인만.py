4710233fd789de4388f143d520eadf7a41cc1649
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-02-18 23:08:09 +0900

    [Bronze III] Title: 파인만, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5724. 파인만/파인만.py
@@ -0,0 +1,12 @@
+input = open(0).readline
+
+def find_squares(size):
+    square_cnt = 0
+
+    for n in range(1, size+1):
+        square_cnt += pow(size+1-n, 2)
+    
+    return square_cnt
+
+while (size:=int(input())):
+    print(find_squares(size))
