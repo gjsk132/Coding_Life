36407d606b201f8862b7c7e635fd6c890f0e2614
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-02-20 17:48:36 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+input = open(0).readline
+
+heights = [int(input()) for _ in range(9)]
+
+for combi in combinations(range(9), 7):
+    if sum([heights[i] for i in combi]) == 100:
+        for h in sorted([heights[i] for i in combi]):
+            print(h)
+        break
