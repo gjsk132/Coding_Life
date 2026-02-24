4d7efac560f3dfb358207a38006825b14d95aca2
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-02-24 14:15:34 +0900

    [Bronze II] Title: 피시방 알바, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -0,0 +1,15 @@
+input = open(0).readline
+
+seats = [0]*101
+
+peoples = int(input())
+
+reject_cnt = 0
+
+for pos in list(map(int, input().split())):
+    if not seats[pos]:
+        seats[pos] = 1
+    else:
+        reject_cnt += 1
+
+print(reject_cnt)
