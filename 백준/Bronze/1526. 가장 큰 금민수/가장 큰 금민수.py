0e1c3f4f24b06f913f91bbbe4a4819ccbcf3f96f
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-02-28 19:15:21 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 168 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -0,0 +1,20 @@
+input = open(0).readline
+
+no_num = ['0', '1', '2', '3', '5', '6', '8', '9']
+
+num = int(input())
+
+while True:
+
+    flag = True
+
+    for n in str(num):
+        if n in no_num:
+            flag = False
+            break
+    
+    if flag:
+        print(num)
+        break
+    
+    num -= 1
