50f318fd4d1739f6e4b56bc2d9cf1a7768561ba5
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-03-16 16:31:26 +0900

    [Silver V] Title: Gravity Hackenbush, Time: 316 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25335. Gravity Hackenbush/Gravity Hackenbush.py
@@ -0,0 +1,23 @@
+input = open(0).readline
+
+n, l = map(int, input().split())
+
+red, blue, green = 0, 0, 0
+
+for _ in range(n):
+    input()
+
+for _ in range(l):
+    _s, _e, color = input().split()
+
+    if color == "R":
+        red += 1
+    elif color == "B":
+        blue += 1
+    else:
+        green += 1
+
+if (red == blue):
+    print("jhnah917" if green%2 else "jhnan917")
+else:
+    print("jhnah917" if red > blue else "jhnan917")
