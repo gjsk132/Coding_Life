6a6102e0b6578b06cbddb50207a521c6bf4e9a63
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2026-03-18 14:40:57 +0900

    [Bronze III] Title: 미국 스타일, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2712. 미국 스타일/미국 스타일.py
@@ -0,0 +1,15 @@
+input = open(0).readline
+
+cases = int(input())
+
+offset = {
+    "kg" : ("lb", 2.2046),
+    "lb" : ("kg", 0.4536),
+    "l" : ("g", 0.2642),
+    "g" : ("l", 3.7854)
+}
+
+for _ in range(cases):
+    amount, unit = input().split()
+    
+    print(f"{offset[unit][1]*float(amount):.4f} {offset[unit][0]}")
